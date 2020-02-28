679ff9623c2f774f637b9277011a5cf4388ff9e5
lucassousaf <lucas.sousa@magnet.coop>
2020-02-28 13:42:57 +0100

    Prepare for next development release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject magnet/sql-utils "0.4.8"
+(defproject magnet/sql-utils "0.4.9-SNAPSHOT"
   :description "A library designed as a thin convenience wapper over clojure.java.jdbc"
   :url "https://github.com/magnetcoop/sql-utils"
   :license {:name "Mozilla Public Licence 2.0"
